8656fad425f1c3acf3ede79a5ae364c13bfa9bd7
Matt Leung <8941112+mdleung@users.noreply.github.com>
2020-03-28 09:17:56 -0700

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -47,7 +47,7 @@ export const pageQuery = graphql`
           tags
           heroImage {
             fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-              ...GatsbyContentfulFluid
+              ...GatsbyContentfulFluid_tracedSVG
           }
           description {
             childMarkdownRemark {
